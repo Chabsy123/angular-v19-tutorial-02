f1a24ae68a9ed0815d9504cbf1f5495b443d9768
Chukwu Sydney <chukwusydney061@gmail.com>
2025-08-15 17:18:51 +0100

    Introduce Directives

---
+++ src/app/directives/directives.component.ts
@@ -0,0 +1,14 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { DataBindingComponent } from '../data-binding/data-binding.component';
+
+@Component({
+  selector: 'app-directives',
+  // to use data-binding components and ngIf
+  imports: [],
+  templateUrl: './directives.component.html',
+  styleUrl: './directives.component.scss'
+})
+export class DirectivesComponent {
+
+}
