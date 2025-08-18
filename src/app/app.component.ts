5ba95c1311c8e8c9dec2d0bd63f85759e1f7e85a
Chukwu Sydney <chukwusydney061@gmail.com>
2025-08-18 08:44:36 +0100

    Introduce @Switch to Supersede ngSwitch Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { RouterOutlet } from '@angular/router';
 import { DirectivesComponent } from './directives/directives.component';
 import { StructuralDirectivesNgifVsIfComponent } from './structural-directives-ngif-vs-if/structural-directives-ngif-vs-if.component';
 import { StructuralDirectiveNgforVsForComponent } from './structural-directive-ngfor-vs-for/structural-directive-ngfor-vs-for.component';
+import { StructuralDirectiveNgswitchVsSwitchComponent } from './structural-directive-ngswitch-vs-switch/structural-directive-ngswitch-vs-switch.component';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,10 @@ import { StructuralDirectiveNgforVsForComponent } from './structural-directive-n
     RouterOutlet,
     // UserComponent,
     // DataBindingComponent,
-    DirectivesComponent,
-    StructuralDirectivesNgifVsIfComponent,
-    StructuralDirectiveNgforVsForComponent
+    // DirectivesComponent,
+    // StructuralDirectivesNgifVsIfComponent,
+    // StructuralDirectiveNgforVsForComponent,
+    StructuralDirectiveNgswitchVsSwitchComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
